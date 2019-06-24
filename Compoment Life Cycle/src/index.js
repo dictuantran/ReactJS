10dd36f2ddaa3e2c8c0d07ff7c1a5e12292c77c5
TUAN TRAN <dictuantran@gmail.com>
2019-06-24 21:39:21 +0700

    Delete all

--- Compoment Life Cycle/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import App from './components/App.jsx';
-import App from './components/ComponentLifeCycle.jsx';
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
-setTimeout(()=> {
-    ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
