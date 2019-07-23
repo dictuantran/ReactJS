e941d9de6bf31c1e9704ca7a5b4cea031f09c719
TUAN TRAN <dictuantran@gmail.com>
2019-07-23 21:33:12 +0700

    create product list for admin

--- sportsstore/src/admin/product/ProductsTable.js
+++ sportsstore/src/admin/product/ProductsTable.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { PaginationControls } from "../../components/controls/PaginationControls";
+import { ProductsRow } from "../product/ProductsRow";
+import { Link } from "react-router-dom";
 
 export class ProductTable extends Component {
 
@@ -6,5 +9,28 @@ export class ProductTable extends Component {
         <h4 className="bg-info text-white text-center p-2">
             { this.props.totalSize } Products
         </h4>
+        <PaginationControls keys={["ID", "Name", "Category"]}
+            { ...this.props } />
+
+        <table className="table table-sm table-striped">
+            <thead>
+                <tr><th>ID</th>
+                    <th>Name</th><th>Category</th>
+                    <th className="text-right">Price</th>
+                    <th className="text-center"></th>
+                </tr>
+            </thead>
+            <tbody>
+                { this.props.products.map(prod => 
+                    <ProductsRow key={ prod.id} product={ prod }
+                        deleteProduct={ this.props.deleteProduct } />
+                )}
+            </tbody>
+        </table>
+        <div className="text-center">
+            <Link to="/admin/products/create" className="btn btn-primary">
+                Create Product
+            </Link>
+        </div>
     </div>
 }
